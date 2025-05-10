a310cad5db688f4189971dc223e47aeb1a2cf6cf
Manyata39 <manyatasingh2805@gmail.com>
2025-05-10 18:23:50 +0530

    first commit

---
+++ src/Mycomponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  //let footerstyle={
+    //position: "absolute",
+    //top: "50vh",
+    //width:"100%"
+  //}
+  return (
+    <footer className='bg-dark text-light py-3'>
+      <p className='text-center'>
+        Copyright &copy; MytodosList.com
+      </p>
+    </footer>
+  )
+}
+
